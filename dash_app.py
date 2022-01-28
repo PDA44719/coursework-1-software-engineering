2a611a5d5570ee1d4a8d9d4e624d3dd77522c1b2
Pablo De Dios <zcee4pde@ucl.ac.uk>
2022-01-28 19:03:08 +0000

    Introduced fig3, fig4 and fig5. Included create_card and create_page_with_dropdown functions to avoid repetition.

--- dash_app.py
+++ dash_app.py
@@ -76,66 +76,89 @@ for i in range(len(genres)):
                           'Overall Genre Revenue': 'Overall Genre Revenue (USD)'},
                   )
 
-# Create the card containing the first image
-card1 = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Div([
-                    html.H5('Revenue (per Movie) of Each Genre', className="card-title"),
-                    dbc.CardImg(src='assets/graph1.png'),
-                    html.P(
-                        "Some quick example text to build on the card title and "
-                        "make up the bulk of the card's content.",
-                        className="card-text",
-                    ),
-                    dbc.Button("See Graph", color="primary", href='graph-page'),
-                ])
-            ]
-        ),
-    ],
-)
+    fig3 = px.histogram(df, x='Runtime', y='Revenue', log_y=True, nbins=10)
+    fig4 = px.histogram(df, x='Runtime', y='Revenue', histfunc='avg', log_y=True, nbins=10)
+    fig5 = px.histogram(df, x='Runtime', nbins=10)
+
+
+# Create a function that contains the cards containing the images
+def create_card(image_source, button_url):
+    card = dbc.Card(
+        [
+            dbc.CardBody(
+                [
+                    html.Div([
+                        html.H5('Revenue (per Movie) of Each Genre', className="card-title"),
+                        dbc.CardImg(src=image_source),
+                        html.P(
+                            "Some quick example text to build on the card title and "
+                            "make up the bulk of the card's content.",
+                            className="card-text",
+                        ),
+                        dbc.Button("See Graph", color="primary", href=button_url),
+                    ])
+                ]
+            ),
+        ],
+    )
+    return card
+
 
 main_page_layout = html.Div([
     html.H1(children='Dashboard'),
     html.Div(id='main_page_content'),
     dbc.Row([
-        dbc.Col([card1], width=3), dbc.Col([card1], width=3), dbc.Col([card1], width=3), dbc.Col([card1], width=3),
-        dbc.Col([card1], width=3), dbc.Col([card1], width=3)
+        dbc.Col([create_card('assets/graph1.png', 'graph-page-1')], width=3),
+        dbc.Col([create_card('assets/graph2.png', 'graph-page-2')], width=3),
+        dbc.Col([create_card('assets/graph1.png', 'graph-page-1')], width=3),
+        dbc.Col([create_card('assets/graph1.png', 'graph-page-1')], width=3),
+        dbc.Col([create_card('assets/graph1.png', 'graph-page-1')], width=3),
+        dbc.Col([create_card('assets/graph1.png', 'graph-page-1')], width=3)
     ]),
 ])
 
-graph_page_layout = html.Div([
-    html.H1(children='Specific Graph'),
-    html.Div(id='graph_page_content'),
-    dbc.Row([
-        dbc.Col([
-            dcc.Dropdown(
-                id='dropdown1',
-                options=[
-                    {'label': 'Genre Revenue per Movie', 'value': 'type1'},
-                    {'label': 'Overall Genre Revenue', 'value': 'type2'}
-                ],
-                value='type1',  # Starting on the Genre Revenue per Move
-                clearable=False  # Do not allow the dropdown value to be None
-            ),
-        ], width={"size": 6, "offset": 3})
-    ]),
-    dbc.Row([
-        dbc.Col([dcc.Graph(id='graph_1')], width={"size": 6, "offset": 3})
-    ]),
-    dbc.Row([
-        dbc.Col([dbc.Button("Go back to main page", color='primary', href='main-page')], width={"size": 4, "offset": 8})
+
+def create_graph_page_with_dropdown(graph_id, dropdown_id, dropdown_options, starting_value):
+    graph_page_layout = html.Div([
+        html.H1(children='Specific Graph'),
+        html.Div(),
+        dbc.Row([
+            dbc.Col([
+                dcc.Dropdown(
+                    id=dropdown_id,
+                    options=dropdown_options,
+                    value=starting_value,  # Starting on the Genre Revenue per Move
+                    clearable=False  # Do not allow the dropdown value to be None
+                ),
+            ], width={"size": 6, "offset": 3})
+        ]),
+        dbc.Row([
+            dbc.Col([dcc.Graph(id=graph_id)], width={"size": 6, "offset": 3})
+        ]),
+        dbc.Row([
+            dbc.Col([dbc.Button("Go back to main page", color='primary', href='main-page')],
+                    width={"size": 4, "offset": 8})
+        ])
     ])
-])
+
+    return graph_page_layout
 
 
 # Go through the different pages
 @app.callback(Output('page-content', 'children'),
               [Input('url', 'pathname')])
 def navigate_pages(pathname):
-    if pathname == '/graph-page':
-        return graph_page_layout
+    if pathname == '/graph-page-1':
+        dropdown_options = [{'label': 'Genre Revenue per Movie', 'value': 'type1_1'},
+                            {'label': 'Overall Genre Revenue', 'value': 'type1_2'}]
+        return create_graph_page_with_dropdown('graph_1', 'dropdown1', dropdown_options, 'type1_1')
+
+    if pathname == '/graph-page-2':
+        dropdown_options = [{'label': 'Sum of Revenue', 'value': 'type2_1'},
+                            {'label': 'Average of Revenue', 'value': 'type2_2'},
+                            {'label': 'Number of movies', 'value': 'type2_3'}]
+        return create_graph_page_with_dropdown('graph_2', 'dropdown2', dropdown_options, 'type2_1')
+
     else:
         return main_page_layout
 
@@ -144,10 +167,22 @@ def navigate_pages(pathname):
 @app.callback(Output('graph_1', component_property='figure'),
               [Input('dropdown1', 'value')])
 def change_y_axis(value):
-    if value == 'type1':
+    if value == 'type1_1':
         return fig1
     else:
         return fig2
+
+
+# Select the y axis for graph 1
+@app.callback(Output('graph_2', component_property='figure'),
+              [Input('dropdown2', 'value')])
+def change_y_axis(value):
+    if value == 'type2_1':
+        return fig3
+    elif value == 'type2_2':
+        return fig4
+    else:
+        return fig5
 
 
 app.layout = html.Div([
